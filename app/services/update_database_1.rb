387ddfa72e976c670bba151ed34a40db5b895449
Scott Weaver <scott12@fastmail.fm>
2019-04-15 01:22:02 +0000

    readiing fill_database for stream 1 for production

--- app/services/update_database_1.rb
+++ app/services/update_database_1.rb
@@ -1,4 +1,3 @@
-sleep(20)
 ds_1 = DeltaStream.find_by_id(1)
 ds_1 ||= DeltaStream.create(id: 1, frequency_minutes: 3, delta_reachback: 6)
 ds_1.create_pretty_response_file_and_fill_database
--- fill_database_1.sh
+++ fill_database_1.sh
@@ -1,3 +1,1 @@
-
-cd /home/scott/dev/nds/ndsapp1/
 rails runner "app/services/update_database_1.rb"
