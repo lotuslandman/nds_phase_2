8396c9dad18df6ea51f8c2ba47a48a7b5a26d057
Scott Weaver <scott12@fastmail.fm>
2019-04-18 03:55:29 +0000

    removing services

--- app/services/update_database_2.rb
+++ app/services/update_database_2.rb
@@ -1,5 +1,4 @@
-sleep(5)
-ds_2 = DeltaStream.find_by_id(2)
-ds_2 ||= DeltaStream.create(id: 2, frequency_minutes: 3, delta_reachback: 6)
-ds_2.create_pretty_response_file_and_fill_database
+#ds_2 = DeltaStream.find_by_id(2)
+#ds_2 ||= DeltaStream.create(id: 2, frequency_minutes: 3, delta_reachback: 6)
+#ds_2.create_pretty_response_file_and_fill_database
     
